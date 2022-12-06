1a795fe88d12acd7f227bc7b03089572eddfaa10
Matthew Stanley <matthew.stanley35t@gmail.com>
2022-12-06 08:17:06 -0700

    adding project updates

--- js/weather_js.js
+++ js/weather_js.js
@@ -31,21 +31,21 @@ function getWeather(lat,long){
         for(let i = 0; i < reports.length; i += 8) {
             console.log(reports[i]);
 
-            let cardHead = reports[i].dt_txt.split(' ');
-            let highTemp = reports[i].main.temp_max;
-            let lowTemp = reports[i].main.temp_min;
-            let iconCode = reports[i].weather[0].icon;
-            let weatherDescription = reports[i].weather[0].description;
-            let humid = reports[i].main.humidity;
+            let cardHeader = reports[i].dt_txt.split(' ');
+            let highT = reports[i].main.temp_max;
+            let lowT = reports[i].main.temp_min;
+            let icon = reports[i].weather[0].icon;
+            let weatherDesc = reports[i].weather[0].description;
+            let humidity = reports[i].main.humidity;
             let windSpeed = reports[i].wind.speed;
             let pressure = reports[i].main.pressure;
 
             html +=
-                '<div class="card text-center" style="width: 18rem;">' +
-                '<div class="card-header">' + cardHead[0] + '</div>' +
+                '<div class="card text-center" style="width: 15rem;">' +
+                '<div class="card-header">' + cardHeader[0] + '</div>' +
                 '<ul class="list-group list-group-flush">' +
-                '<li class="list-group-item"><span>' + highTemp + '*F / ' + lowTemp + '*F</span><br><img src="https://openweathermap.org/img/w/' + iconCode + '.png" alt="Weather Icon"></li>'+
-                '<li class="list-group-item"><span>Description: ' + weatherDescription + '</span><br><span>Humidity: ' + humid + '%</span></li>'+
+                '<li class="list-group-item"><span>' + highT + '*F / ' + lowT + '*F</span><br><img src="https://openweathermap.org/img/w/' + icon + '.png" alt="Weather Icon"></li>'+
+                '<li class="list-group-item"><span>Description: ' + weatherDesc + '</span><br><span>Humidity: ' + humidity + '%</span></li>'+
                 '<li class="list-group-item">Wind Speed: ' + windSpeed + 'mph</li>'+
                 '<li class="list-group-item">Pressure: ' + pressure + 'psi</li>'+
                 '</ul>'+
